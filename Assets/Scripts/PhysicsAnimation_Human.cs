73a6e061fc35c9516b970d6c1e14d26af08d5a12
Jason Miletta <jmiletta01@gmail.com>
2018-07-14 23:50:30 -0500

    Add rigidbody masses, update directional walking and limb swinging

--- Assets/Scripts/PhysicsAnimation_Human.cs
+++ Assets/Scripts/PhysicsAnimation_Human.cs
@@ -12,25 +12,32 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 	public float standingForce = 1.0f;
 	public float standingHeight = 1.0f;
 	public float uprightTorque = 5f;
-	public float forwardTorque = 5f;
+	public float forwardFacingTorque = 5f;
 	#endregion
 
 	[Header("Components")]
 	#region Components
 	public Rigidbody hips;
 	public Rigidbody spineMid;
+	public Rigidbody leftThigh;
 	public Rigidbody leftKnee;
 	public Rigidbody leftFoot;
+	public Rigidbody leftHand;
+	public Rigidbody rightThigh;
 	public Rigidbody rightKnee;
 	public Rigidbody rightFoot;
+	public Rigidbody rightHand;
 	#endregion
 
 	#region State
+	[Header("State")]
 	public AnimationState currentAnimationState = AnimationState.Standing;
 	public Vector3 currentFloorPosition;
 	public Vector3 forwardFacingTargetVector;
 	[SerializeField]
-	private float stepSpeed;
+	private float walkSpeed = 1.0f;
+	[SerializeField]
+	private float stepPace;
 	private float currentStepTime;
 	private bool stepWithLeftLeg = false;
 	[SerializeField]
@@ -39,6 +46,7 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 	
     #region GIZMOS
     List<Ray> raysToDraw = new List<Ray>();
+	List<Vector3> spheresToDraw = new List<Vector3>();
     #endregion
     void OnDrawGizmos(){
         foreach(Ray r in raysToDraw){
@@ -46,7 +54,12 @@ public class PhysicsAnimation_Human : MonoBehaviour {
         }
 		raysToDraw = new List<Ray>();
 
-		Gizmos.DrawWireSphere(getCurrentFloorPosition(), 0.1f);
+		spheresToDraw.Add(getCurrentFloorPosition());
+		foreach(Vector3 v in spheresToDraw){
+			Gizmos.DrawWireSphere(v, 0.1f);			
+		}
+		spheresToDraw = new List<Vector3>();
+
 		Gizmos.DrawRay(hips.transform.position, forwardFacingTargetVector);
     }
 
@@ -60,8 +73,10 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		Ray ray = new Ray (spineMid.transform.position, Vector3.down);
+		raysToDraw.Add(ray);
         RaycastHit hit;
-		bool didHit = Physics.Raycast(ray, out hit);
+		bool didHit = Physics.Raycast(ray, out hit, standingHeight, (1 << LayerMask.NameToLayer("Terrain")));
+		spheresToDraw.Add(hit.point);
 		uprightRotationalCorrectionUpdate();
 		forwardFacingRotationalCorrectionUpdate();
 		standingUpdate(didHit, hit, spineMid.transform.position);
@@ -77,13 +92,17 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 		} else {
 			currentAnimationState = AnimationState.Walking;
 		}
+
+		forwardFacingTargetVector = movementVector;
+		hips.AddForce(movementVector * walkSpeed, ForceMode.Acceleration);
 	}
 	
 	private void standingUpdate(bool didHit, RaycastHit hit, Vector3 standingPosition){
 		if(didHit){
+			spheresToDraw.Add(hit.point);
             float proportionalHeight = (standingHeight - hit.distance) / standingHeight;
             Vector3 appliedStandingForce = Vector3.up * proportionalHeight * standingForce;
-            spineMid.AddForceAtPosition(appliedStandingForce, standingPosition, ForceMode.Force);
+            spineMid.AddForce(appliedStandingForce, ForceMode.Force);
             raysToDraw.Add(new Ray(standingPosition, appliedStandingForce));
 		}
 	}
@@ -91,23 +110,35 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 	private void walkingUpdate(bool didHit, RaycastHit hit, Vector3 position){
 		if(didHit){
 			currentStepTime += Time.deltaTime;
-			if(currentStepTime > stepSpeed){
+			if(currentStepTime > stepPace){
 				currentStepTime = 0.0f;
 				stepWithLeftLeg = !stepWithLeftLeg;
 			}
-			Vector3 upForwardVector = new Vector3(0.0f, 1.0f, 1.0f * legStrength);
-			Vector3 backVector =  -Vector3.forward * legStrength;
+			
+			Vector3 upForwardVector = forwardFacingTargetVector * legStrength;
+			Vector3 backVector =  -forwardFacingTargetVector * legStrength;
 			if(stepWithLeftLeg){
 				//Step With left leg
 				leftKnee.AddForce(upForwardVector, ForceMode.Force);
+				leftThigh.AddForce(upForwardVector, ForceMode.Force);
+				leftHand.AddForce(backVector * 0.5f, ForceMode.Force);
+
 				rightKnee.AddForce(backVector, ForceMode.Force);
+				rightThigh.AddForce(backVector, ForceMode.Force);
+				rightHand.AddForce(upForwardVector * 0.5f, ForceMode.Force);
 				
 				raysToDraw.Add(new Ray(leftKnee.transform.position, upForwardVector));
 				raysToDraw.Add(new Ray(rightKnee.transform.position,backVector));
 			} else {
 				//Step With right leg
 				rightKnee.AddForce(upForwardVector, ForceMode.Force);
+				rightThigh.AddForce(upForwardVector, ForceMode.Force);
+				rightHand.AddForce(backVector * 0.5f, ForceMode.Force);
+
 				leftKnee.AddForce(backVector, ForceMode.Force);
+				leftThigh.AddForce(backVector, ForceMode.Force);
+				leftHand.AddForce(upForwardVector * 0.5f, ForceMode.Force);
+
 				raysToDraw.Add(new Ray(rightKnee.transform.position, upForwardVector));
 				raysToDraw.Add(new Ray(leftKnee.transform.position,backVector));
 			}
@@ -122,10 +153,10 @@ public class PhysicsAnimation_Human : MonoBehaviour {
 
 	private void forwardFacingRotationalCorrectionUpdate(){
 		var hipsRotation = Quaternion.FromToRotation(hips.transform.forward, forwardFacingTargetVector);
- 		hips.AddTorque(new Vector3(hipsRotation.x, hipsRotation.y, hipsRotation.z)*forwardTorque);
+ 		hips.AddTorque(new Vector3(hipsRotation.x, hipsRotation.y, hipsRotation.z)*forwardFacingTorque);
 		 
 		var spineMidRotation = Quaternion.FromToRotation(spineMid.transform.forward, forwardFacingTargetVector);
- 		spineMid.AddTorque(new Vector3(spineMidRotation.x, spineMidRotation.y, spineMidRotation.z)*forwardTorque);
+ 		spineMid.AddTorque(new Vector3(spineMidRotation.x, spineMidRotation.y, spineMidRotation.z)*forwardFacingTorque);
 	}
 
 	private Vector3 getCurrentFloorPosition(){
