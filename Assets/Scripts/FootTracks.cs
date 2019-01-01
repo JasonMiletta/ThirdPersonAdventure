8aea23fc1cdf693b0b488b4538e4b88506d643f5
Jason Miletta <jmiletta01@gmail.com>
2019-01-01 14:58:50 -0600

    Update SnowTracks to be on the plane objects and have footTracks fire the events

--- Assets/Scripts/FootTracks.cs
+++ Assets/Scripts/FootTracks.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class FootTracks : MonoBehaviour
 {
-    public Shader drawShader;
-    public GameObject terrain;
     public Transform[] foot;
     public float tracksHeightDistance = 1;
 
@@ -15,11 +13,6 @@ public class FootTracks : MonoBehaviour
     public float brushStrength = 1;
     private int layerMask;
     RaycastHit terrainHit;
-
-    private Material drawMaterial;
-    private Material myMaterial;
-
-    private RenderTexture splatMap;
 
     #region GIZMOS
     List<Ray> raysToDraw = new List<Ray>();
@@ -33,11 +26,7 @@ public class FootTracks : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
-        layerMask = LayerMask.GetMask("Terrain");
-        drawMaterial = new Material(drawShader);
-        myMaterial = terrain.GetComponent<MeshRenderer>().material;
-        myMaterial.SetTexture("_Splat", splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat));      
+    {    
     }
 
     // Update is called once per frame
@@ -47,13 +36,10 @@ public class FootTracks : MonoBehaviour
 			Ray ray = new Ray (foot.position, new Vector3(0, -tracksHeightDistance, 0));
             raysToDraw.Add(ray);
             if(Physics.Raycast(ray, out terrainHit, tracksHeightDistance, (1 << LayerMask.NameToLayer("Terrain")))){
-                drawMaterial.SetVector("_Coordinate", new Vector4(terrainHit.textureCoord.x, terrainHit.textureCoord.y, 0, 0));
-                drawMaterial.SetFloat("_Strength", brushStrength);
-                drawMaterial.SetFloat("_Size", brushSize);
-                RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height, 0, RenderTextureFormat.ARGBFloat);
-                Graphics.Blit(splatMap, temp);
-                Graphics.Blit(temp, splatMap, drawMaterial);
-                RenderTexture.ReleaseTemporary(temp);
+                SnowPrintTracker snowTracker = terrainHit.collider.GetComponent<SnowPrintTracker>();
+                if(snowTracker){
+                    snowTracker.drawTracksToMaterial(terrainHit, brushSize, brushStrength);
+                }
             }
         }
     }
---
+++ Assets/Scripts/SnowPrintTracker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowPrintTracker : MonoBehaviour
+{
+    public Shader drawShader;
+    public float tracksHeightDistance = 1;
+
+    private int layerMask;
+    RaycastHit terrainHit;
+
+    private Material drawMaterial;
+    private Material myMaterial;
+
+    private RenderTexture splatMap;
+
+    #region GIZMOS
+    List<Ray> raysToDraw = new List<Ray>();
+    #endregion
+    void OnDrawGizmos(){
+        foreach(Ray r in raysToDraw){
+            Gizmos.DrawRay(r);
+        }
+		raysToDraw = new List<Ray>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        layerMask = LayerMask.GetMask("Terrain");
+        drawMaterial = new Material(drawShader);
+        myMaterial = GetComponent<MeshRenderer>().material;
+        myMaterial.SetTexture("_Splat", splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat));      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    public void drawTracksToMaterial(RaycastHit terrainHit, float brushSize, float brushStrength){
+        drawMaterial.SetVector("_Coordinate", new Vector4(terrainHit.textureCoord.x, terrainHit.textureCoord.y, 0, 0));
+        drawMaterial.SetFloat("_Strength", brushStrength);
+        drawMaterial.SetFloat("_Size", brushSize);
+        RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height, 0, RenderTextureFormat.ARGBFloat);
+        Graphics.Blit(splatMap, temp);
+        Graphics.Blit(temp, splatMap, drawMaterial);
+        RenderTexture.ReleaseTemporary(temp);
+    }
+}
